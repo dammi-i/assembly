5bba17370eff1b106c471fbb7811d110f6814a49
Dammirbuh <walidakka53@gmail.com>
2021-03-17 12:42:27 +0100

    .

--- add.s
+++ add.s
@@ -1,15 +1,9 @@
 .globl _start
 
 _start:
-    mov x0, #1
-    mov x4, '1'
-    mov x5, '2'
-    add x6, x4, x5
-    ldr x1, [x6]
-    ldr x2, #4
-    mov w8, #64
-    svc #0
-
-    mov x0, #0
-    mov w8, #93
+    mov x1, #1
+    mov x2, #2
+    add x6, x1, x2
+    mov x0, x6
+    mov w8, #0x5D
     svc #0
